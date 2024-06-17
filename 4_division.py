3cdb1b51adf1e8c2701361299b5b929bf99f52e8
Eren <86615310+erent8@users.noreply.github.com>
2024-06-18 00:39:04 +0300

    Create 4_division.py

---
+++ 4_division.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    int_bolum = a // b
+    float_bolum = a / b
+    
+    print(int_bolum)
+    print(float_bolum)
+    
+    
+    
+    
