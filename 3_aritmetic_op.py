b6626e124c254f662cd3b0f6e429ff404270864c
Eren <86615310+erent8@users.noreply.github.com>
2024-06-18 00:32:43 +0300

    Create 3_aritmetic_operators.py

---
+++ 3_aritmetic_op.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    toplama = a + b
+
+    cikarma = a - b 
+
+    carpma = a * b
+
+
+    print(toplama)
+    print(cikarma)
+    print(carpma)
