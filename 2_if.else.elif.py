c50cff7cf503a4d671a31f946d8cbd7a550a0b67
Eren <86615310+erent8@users.noreply.github.com>
2024-09-01 13:35:49 +0300

    Update 2_if.else.elif.py

--- 2_if.else.elif.py
+++ 2_if.else.elif.py
@@ -14,5 +14,5 @@ elif n % 2 == 0 and 2 <= n <= 5:
     print("Not Weird")
 elif n % 2 == 0 and 6 <= n <= 20:
     print("Weird")
-elif n % 2 == 0 and n > 20:
+elif n % 2 == 0 and n > 20: 
     print("Not Weird")
